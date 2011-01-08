4c50ec7c460b692ab54ef4d8f22b0a0b57eb6e3a
Gé Vissers <ge@ghnet.nl>
2011-01-08 14:06:40 +0000

    std::tolower is a template in modern C++. Use it.

--- support.cc
+++ support.cc
@@ -1,13 +1,18 @@
-#include <cctype>
+#include <locale>
+#include <algorithm>
+#include <tr1/functional>
 #include "support.hh"
+
+using namespace std::tr1::placeholders; 
 
 std::string ucFirst(const std::string& str)
 {
 	if (str.empty()) return std::string();
 
-	std::string res(1, std::toupper(str[0]));
+	std::locale loc;
+	std::string res(1, std::toupper<char>(str[0], loc));
 	std::transform(str.begin()+1, str.end(), std::back_inserter(res),
-		std::tolower);
+		std::tr1::bind(std::tolower<char>, _1, loc));
 
 	return res;
 }
