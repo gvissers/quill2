bf5efb5d9af3b84425d4273e0768e816d1186597
Gé Vissers <ge@ghnet.nl>
2011-01-31 10:23:20 +0000

    Proxy class for input streams

---
+++ IStreamUser.hh
@@ -0,0 +1,59 @@
+#ifndef ISTREAMUSER_HH
+#define ISTREAMUSER_HH
+
+/*!
+ * \file IStreamUser.hh
+ * \brief Definition of the IStreamUser class
+ */
+
+/*!
+ * \brief Proxy class for an input stream
+ *
+ * Class IStreamUser can act as a base class for classes that require
+ * an embedded istream object of type \a EmbeddedIStream to be fully
+ * initialized before running the contructor of another base class (e.g.
+ * when this second base class needs access to the stream buffer of the
+ * embedded stream). It simply embeds a \a EmbeddedIStream object, and adds
+ * a few functions to access this stream.
+ * \tparam EmbeddedIStream The type of the input stream to embed
+ */
+template <typename EmbeddedIStream>
+class IStreamUser
+{
+	protected:
+		/*!
+		 * \brief Constructor
+		 *
+		 * Create a new IStreamUser object, embedding a default
+		 * constructed EmbeddedIStream.
+		 */
+		IStreamUser(): _is() {}
+		/*!
+		 * \brief Constructor
+		 *
+		 * Create a new IStreamUser object, embedding an
+		 * EmbeddedIstream which uses \a sb for its stream buffer.
+		 * \param sb The stream buffer of the embedded stream
+		 */
+		IStreamUser(std::streambuf* sb): _is(sb) {}
+
+		//! Return the embedded \c istringstream
+		EmbeddedIStream& stream() { return _is; }
+		//! Return the embedded \c istringstream
+		const EmbeddedIStream& stream() const { return _is; }
+
+		//! Set the status flags of the embedded stream to \a state
+		void clear(std::ios::iostate state=std::ios::goodbit)
+		{
+			_is.clear(state);
+		}
+
+		//! Return the string buffer of the embedded stream
+		std::stringbuf *rdbuf() { return _is.rdbuf(); }
+
+	private:
+		//! The embedded stream object
+		EmbeddedIStream _is;
+};
+
+#endif // ISTREAMUSER_HH
