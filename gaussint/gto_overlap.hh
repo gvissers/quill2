3f9d4787a2eb2bd87a8cd850f873ea3b81e2b56b
Gé Vissers <ge@ghnet.nl>
2011-02-11 12:53:29 +0000

    More specializations for overlap integrals. Let's hope we didn't mistype somewhere...

--- gaussint/gto_overlap.hh
+++ gaussint/gto_overlap.hh
@@ -27,7 +27,7 @@ struct OverlapKernel<0, 0, 0, 0, 0, 0>
 		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
 		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d&)
 	{
-        	return exp_ared / (asum*asum.sqrt());
+        	return exp_ared / (asum * asum.sqrt());
 	}
 };
 template <>
@@ -38,7 +38,8 @@ struct OverlapKernel<1, 0, 0, 0, 0, 0>
 		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
 		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
 	{
-		return r.x() * beta * exp_ared / (asum.square()*asum.sqrt());
+		return r.x() * beta * exp_ared
+			/ (asum.square() * asum.sqrt());
 	}
 };
 template <>
@@ -49,7 +50,8 @@ struct OverlapKernel<0, 1, 0, 0, 0, 0>
 		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
 		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
 	{
-		return r.y() * beta * exp_ared / (asum.square()*asum.sqrt());
+		return r.y() * beta * exp_ared
+			/ (asum.square() * asum.sqrt());
 	}
 };
 template <>
@@ -60,7 +62,8 @@ struct OverlapKernel<0, 0, 1, 0, 0, 0>
 		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
 		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
 	{
-		return r.z() * beta * exp_ared / (asum.square()*asum.sqrt());
+		return r.z() * beta * exp_ared
+			/ (asum.square() * asum.sqrt());
 	}
 };
 template <>
@@ -71,7 +74,8 @@ struct OverlapKernel<0, 0, 0, 1, 0, 0>
 		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
 		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
 	{
-		return -r.x() * alpha * exp_ared / (asum.square()*asum.sqrt());
+		return -r.x() * alpha * exp_ared
+			/ (asum.square() * asum.sqrt());
 	}
 };
 template <>
@@ -82,7 +86,8 @@ struct OverlapKernel<0, 0, 0, 0, 1, 0>
 		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
 		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
 	{
-		return -r.y() * alpha * exp_ared / (asum.square()*asum.sqrt());
+		return -r.y() * alpha * exp_ared
+			/ (asum.square() * asum.sqrt());
 	}
 };
 template <>
@@ -93,7 +98,176 @@ struct OverlapKernel<0, 0, 0, 0, 0, 1>
 		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
 		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
 	{
-		return -r.z() * alpha * exp_ared / (asum.square()*asum.sqrt());
+		return -r.z() * alpha * exp_ared
+			/ (asum.square() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<2, 0, 0, 0, 0, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd& beta,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return (0.5*asum + r.x()*r.x()*beta.square()) * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<1, 1, 0, 0, 0, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd& beta,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return r.x() * r.y() * beta.square() * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<1, 0, 1, 0, 0, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd& beta,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return r.x() * r.z() * beta.square() * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<1, 0, 0, 1, 0, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd& ared,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return (0.5 - r.x()*r.x()*ared) * exp_ared
+			/ (asum.square() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<1, 0, 0, 0, 1, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd& ared,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return -r.x() * r.y() * ared * exp_ared
+			/ (asum.square() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<1, 0, 0, 0, 0, 1>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd& ared,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return -r.x() * r.z() * ared * exp_ared
+			/ (asum.square() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 2, 0, 0, 0, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd& beta,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return (0.5*asum + r.y()*r.y()*beta.square()) * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 1, 1, 0, 0, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd& beta,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return r.y() * r.z() * beta.square() * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 1, 0, 1, 0, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd& ared,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return -r.y() * r.x() * ared * exp_ared
+			/ (asum.square() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 1, 0, 0, 1, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd& ared,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return (0.5 - r.y()*r.y()*ared) * exp_ared
+			/ (asum.square() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 1, 0, 0, 0, 1>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd& ared,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return -r.y() * r.z() * ared * exp_ared
+			/ (asum.square() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 0, 2, 0, 0, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd& beta,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return (0.5*asum + r.z()*r.z()*beta.square()) * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 0, 1, 1, 0, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd& ared,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return -r.z() * r.x() * ared * exp_ared
+			/ (asum.square() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 0, 1, 0, 1, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd&, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd& ared,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return -r.z() * r.y() * ared * exp_ared
+			/ (asum.square() * asum.sqrt());
 	}
 };
 template <>
@@ -104,10 +278,84 @@ struct OverlapKernel<0, 0, 1, 0, 0, 1>
 		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd& ared,
 		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
 	{
-		return (0.5 - r.z()*r.z()*ared)
-			* exp_ared / (asum.square()*asum.sqrt());
+		return (0.5 - r.z()*r.z()*ared) * exp_ared
+			/ (asum.square() * asum.sqrt());
 	}
 };
+template <>
+struct OverlapKernel<0, 0, 0, 2, 0, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd& alpha, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return (0.5*asum + r.x()*r.x()*alpha.square()) * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 0, 0, 1, 1, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd& alpha, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return r.x() * r.y() * alpha.square() * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 0, 0, 1, 0, 1>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd& alpha, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return r.x() * r.z() * alpha.square() * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 0, 0, 0, 2, 0>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd& alpha, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return (0.5*asum + r.y()*r.y()*alpha.square()) * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+template <>
+struct OverlapKernel<0, 0, 0, 0, 1, 1>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd& alpha, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return r.y() * r.z() * alpha.square() * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+
+template <>
+struct OverlapKernel<0, 0, 0, 0, 0, 2>
+{
+	static Eigen::ArrayXXd calc(
+		const Eigen::ArrayXXd& alpha, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& asum, const Eigen::ArrayXXd&,
+		const Eigen::ArrayXXd& exp_ared, const Eigen::Vector3d& r)
+	{
+		return (0.5*asum + r.z()*r.z()*alpha.square()) * exp_ared
+			/ (asum.cube() * asum.sqrt());
+	}
+};
+
 template <int lz1>
 struct OverlapKernel<0, 0, lz1, 0, 0, 0>
 {
