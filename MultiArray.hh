43d28870b59ecf144787e84fd028ae66a64ce52e
Gé Vissers <ge@octonion.nl>
2012-03-08 17:46:31 +0100

    Actually add the new class

---
+++ MultiArray.hh
@@ -0,0 +1,60 @@
+#ifndef MULTIARRAY_HH
+#define MULTIARRAY_HH
+
+#include <Eigen/Core>
+
+/*!
+ * \brief Class to store multiple matrices
+ *
+ * Class MultiArray stores multiple two-dimenional arrays in a single, bigger
+ * array, making sure that each subarray is aligned on an appropriate memory
+ * boundary.
+ */
+class MultiArray
+{
+	//! The number of elements in a single (SSE) packet
+	static const int pkt_size = Eigen::internal::packet_traits<double>::size;
+
+public:
+	//! Return type of a sub-array
+	typedef Eigen::ArrayXXd::AlignedMapType Block;
+	//! Read-only type of a sub-array
+	typedef Eigen::ArrayXXd::ConstAlignedMapType ConstBlock;
+
+	/*!
+	 * \brief Constructor
+	 *
+	 * Create a new MultiArray object that can hold \a m matrices of size
+	 * \a n1 x \a n2.
+	 * \param n1 The number of rows in each sub-array
+	 * \param n2 The number of columns in each sub-array
+	 * \param m  The number of sub-arrays in this object
+	 */
+	MultiArray(int n1, int n2, int m):
+		_n1(n1), _n2(n2),
+		_nn(pkt_size * ((_n1*_n2+pkt_size-1) / pkt_size)),
+		_A(_nn, m) {}
+
+	//! Return the \a m'th sub-array, writable
+	Block operator[](int m)
+	{
+		return Eigen::ArrayXXd::MapAligned(_A.data()+m*_nn, _n1, _n2);
+	}
+	//! Return the \a m'th sub-array, read-only
+	ConstBlock operator[](int m) const
+	{
+		return Eigen::ArrayXXd::MapAligned(_A.data()+m*_nn, _n1, _n2);
+	}
+
+private:
+	//! The number of rows in each sub-array
+	int _n1;
+	//! The number of columns in each sub-array
+	int _n2;
+	//! The number of elements in each sub-array, rounded up to an entire packet
+	int _nn;
+	//! The actual data matrix
+	Eigen::ArrayXXd _A;
+};
+
+#endif // MULTIARRAY_HH
